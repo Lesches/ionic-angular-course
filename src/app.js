f1a28ddcc384a3292303fcca747472feeb6bf6dd
sholom <sholomls@gmail.com>
2019-05-27 13:59:02 -0400

    app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const reason = document.querySelector('#input-reason');
+const amount = document.querySelector('#input-amount');
+const clear = document.querySelector('#clear');
+const confirm = document.querySelector('#confirm');
+
+confirm.addEventListener('click', () => {
+    console.log('confirmed!');
+});
