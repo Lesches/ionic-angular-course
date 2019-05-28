d373fda910805596cbe43742d59c0aa36646978e
sholom <sholomls@gmail.com>
2019-05-28 19:27:36 -0400

    final commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const clear = document.querySelector('#clear');
 const confirm = document.querySelector('#confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 let totalExpenses = 0;
 
 const clear = () =>{
@@ -15,8 +16,12 @@ confirm.addEventListener('click', () => {
    const enteredReason = reason.value;
    const enteredAmount = amount.value;
 
-   if (enteredReason.trim().length < 1 || enteredAmount <=0 || enteredAmount.trim().length < 1)
+   if (enteredReason.trim().length < 1 || enteredAmount <=0 || enteredAmount.trim().length < 1) {
+       alertCtrl.create({message: 'Please enter valid reason and amount!', header: 'Invalid inputs', buttons: ['Okay']}).then(alertElement =>{
+           alertElement.present();
+       });
        return;
+   }
    const newItem = document.createElement('ion-item');
    newItem.textContent = enteredReason + ": $" + enteredAmount;
 
